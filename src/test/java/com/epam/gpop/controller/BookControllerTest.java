b84eb240572dadcb3bb9ce1e92458b583959985f
Abhijeet Jadhav <ajadhav50@yahoo.com>
2021-07-05 19:23:23 +0530

    add test

---
+++ src/test/java/com/epam/gpop/controller/BookControllerTest.java
@@ -0,0 +1,32 @@
+package com.epam.gpop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Abhijeet Jadhav
+ * 19-06-2021
+ */
+class BookControllerTest {
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+}
